2d04d0bb5b753fbb723523b3fac50b1c4fa6035f
Jeremy Lee <jeremyduanelee@gmail.com>
2013-07-11 17:04:47 -0400

    Initial Commit

---
+++ xslt/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Xsl;
+using System.Xml;
+
+namespace xslt {
+  class Program {
+    static void Main(string[] args) {
+      if (args.Length != 3) {
+        Console.WriteLine(Properties.Resources.ShortDocs);
+        return;
+      }
+      try {
+        XslCompiledTransform transform = new XslCompiledTransform(true);
+        transform.Load(args[1]);
+        transform.Transform(args[0], args[2]);
+        Console.WriteLine("Transform completed successfully.");
+      } catch (XsltException ex) {
+        Console.WriteLine("Exception while processing StyleSheet: " + ex.Message);
+        Console.WriteLine("Position: {0}, {1}", ex.LineNumber, ex.LinePosition);
+        Console.WriteLine("Stack Trace:");
+        Console.WriteLine(ex.StackTrace);
+      } catch (XmlException ex) {
+        Console.WriteLine("Exception while processing StyleSheet: " + ex.Message);
+        Console.WriteLine("Position: {0}, {1}", ex.LineNumber, ex.LinePosition);
+        Console.WriteLine("Stack Trace:");
+        Console.WriteLine(ex.StackTrace);
+      } catch (Exception ex) {
+        Console.WriteLine("Exception: " + ex.Message);
+        Console.WriteLine("Stack Trace:");
+        Console.WriteLine(ex.StackTrace);
+      }
+
+    }
+  }
+}
